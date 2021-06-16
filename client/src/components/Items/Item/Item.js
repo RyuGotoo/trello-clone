9f2c9983a3c3a86d840feacea3e7b434d21474ce
Ryu GOTO <rgoto@Ryus-MacBook-Pro.local>
2021-06-17 00:10:20 +0900

    add edit and delete button to Item

--- client/src/components/Items/Item/Item.js
+++ client/src/components/Items/Item/Item.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const Item = (props) => {
-    return(
+    const handleClick = (e, id) => {
+        const name =  e.target.name
+        if (name === 'edit') {
+            console.log('EDIT', id);
+        } else if (name === 'delete') {
+            console.log('DELETE', id);
+        }
+    }
+
+    return (
         <div>
             <h1>{ props.content }</h1>
             <h3>{ props.detail }</h3>
             <p>{ props.createdAt }</p>
+            <button name="edit" onClick={(e) => handleClick(e, props.id)}>edit</button>
+            <button name="delete" onClick={(e) => handleClick(e, props.id)}>delete</button>
         </div>
     )
 }
